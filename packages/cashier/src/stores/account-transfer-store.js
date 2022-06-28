c83bf41f6e6cb509ad7e48cf1ee5c5d67f960c8e
ashrafali-v <ashrafalifrk@gmail.com>
2022-06-28 12:21:45 +0800

    eslintrc resolver for testing try 6

--- packages/cashier/src/stores/account-transfer-store.js
+++ packages/cashier/src/stores/account-transfer-store.js
@@ -23,7 +23,7 @@ const getSelectedError = (selected_value, is_from_account) => {
     if (is_from_account) {
         return (
             <Localize
-                i18n_default_text='Transfer from {{selected_value}} is not allowed, Please choose another account from dropdownss'
+                i18n_default_text='Transfer from {{selected_value}} is not allowed, Please choose another account from dropdown'
                 values={{ selected_value }}
             />
         );
