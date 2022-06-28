7bf6f84ef63464db608c1d4ed2e5e6703149093d
ashrafali-v <ashrafalifrk@gmail.com>
2022-06-28 10:50:28 +0800

    revert lint config change

--- .eslintrc.js
+++ .eslintrc.js
@@ -75,8 +75,7 @@ module.exports = {
         'import/prefer-default-export': 0,
         'import/extensions': ['warn', 'never', { jsx: 'always', json: 'always' }],
         'no-sequences': ['warn'],
-        // 'import/no-unresolved': [2, { ignore: ['@deriv/components', '@deriv/shared'] }],
-        'import/no-unresolved': 0,
+        'import/no-unresolved': [2, { ignore: ['@deriv/components', '@deriv/shared'] }],
 
         // react rules
         // 'jsx-quotes'                        : ['error', 'prefer-single'],
